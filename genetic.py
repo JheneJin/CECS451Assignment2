809b1ed0209871a9d1a5c25518b617e50247f2bd
JheneJin <80924562+JheneJin@users.noreply.github.com>
2024-02-15 03:34:40 -0800

    Update genetic.py

--- genetic.py
+++ genetic.py
@@ -2,16 +2,6 @@ from board import Board
 import random
 import numpy as np
 
-# def state(board):
-#     stateDic = {}
-#     stringState = ""
-#     for i in range(board.n_queen):
-#         for j in range(board.n_queen):
-#             if board.map[i][j] == 1:
-#                 stringState += str(j + 1)
-#     stateDic[stringState] = stateFitness
-#     return stateDic
-
 def population():
     stateList = []
     for _ in range(8):
@@ -19,31 +9,17 @@ def population():
         stateList.append(stringState)
     return stateList
 
-# # create boards based off the string state then find fitness and append to dic based on state
-# def generateBoard(board, stateList):
-#     stateDic = {}
-#     listIndex = 0
-#     for _ in range(8):
-#         stateBoard = emptyBoard(board)
-#         for i in range(board.n_queen):
-#             valueIndex = stateList[listIndex][i]
-#             valueIndex = int(valueIndex)
-#             stateBoard.map[i][valueIndex] = 1   
-#         fitness = stateBoard.get_fitness()
-#         stateDic[stateList[listIndex]] = fitness
-#         listIndex += 1
-#     return stateDic
-
-def stateGen():
-    stateList = []
-    for _ in range(8):
-        stringState = "".join(str(random.randint(0, 4)) for _ in range(5))
-        stateList.append(stringState)
-    return stateList
+def generateBoard(board, stateStr):
+    columnIndex = 0
+    stateBoard = emptyBoard(board)
+    for i in stateStr:
+        valueIndex = int(i)
+        stateBoard.map[columnIndex][valueIndex] = 1   
+        columnIndex += 1
+    return stateBoard
 
 # create boards based off the string state then find fitness and append to dic based on state
 def getStateFitness(board, stateStr):
-    stateDic = {}
     columnIndex = 0
     stateBoard = emptyBoard(board)
     for i in stateStr:
@@ -75,30 +51,56 @@ def crossover(parents):
    child2 = parent2[:slicePoint] + parent1[slicePoint:]
    return child1, child2
 
-def mutation(children):
+def mutation(children, mutationRate):
     child1, child2 = children
     child1 = list(child1)
     child2 = list(child2)
     childLen = len(child1)
 
-    randIndex1 = random.randint(0,childLen - 1)
-    randIndex2 = random.randint(0,childLen - 1)
+    randDigit1 = random.randint(0, childLen - 1)
+    randDigit2 = random.randint(0, childLen - 1)
 
-    randDigit1 = random.randint(0,childLen - 1)
-    randDigit2 = random.randint(0,childLen - 1)
-
-    child1[randIndex1] = str(randDigit1)
-    child2[randIndex2] = str(randDigit2)
+    for i in range(childLen):
+        if random.random() < mutationRate:
+            child1[i] = str(randDigit1)
+        if random.random() < mutationRate:
+            child2[i] = str(randDigit2)
 
     child1 = "".join(child1)
     child2 = "".join(child2)
     return child1, child2
 
+def fitnessDic(board, population):
+    fitnessDic = {}
+    for i in population:
+        fitness = getStateFitness(board, i)
+        fitnessDic[i] = fitness
+    return fitnessDic
+
+def geneticAlgo(board, generations):
+    arr = population()
+    for generation in range(generations):
+        newGeneration = []
+
+        for _ in range(len(arr) // 2):
+            selected = selection(arr)
+            crossed = crossover(selected)
+            mutated = mutation(crossed, .50)
+            child1, child2 = mutated
+            newGeneration.append(child1)
+            newGeneration.append(child2)
+        
+        fitnessValues = fitnessDic(board, newGeneration)
+        sortedValues = {k: v for k, v in sorted(fitnessValues.items(), key=lambda item: item[1])}
+        bestState =  list(sortedValues.keys())[0]
+        bestFitness = sortedValues[bestState]
+        return bestState, bestFitness
+    
 test = Board(5)
-print("Initial map:")
-for row in test.get_map():
+optimalState, fitness = (geneticAlgo(test, 100000))
+print(optimalState, fitness)
+bestBoard = (generateBoard(test, optimalState))
+for row in bestBoard.get_map():
     print(row)
-print(test.get_fitness())
-# print(state(test))
-arr = population()
-print(generateBoard(test, arr))
+
+
